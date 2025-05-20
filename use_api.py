66d9de3eda4c9eb99f9e1057673adce07de317c4
robert <robert.mueller-uri@gmx.de>
2025-05-20 12:18:07 +0200

    Add timing measurement for API event sending

--- use_api.py
+++ use_api.py
@@ -6,7 +6,10 @@ from load_data import load_csv_data
 filepaths = ["cosmetic_archive/2019-Oct.csv", "cosmetic_archive/2019-Nov.csv"]
 data = load_csv_data(filepaths)
 print("Loaded data: Testing API")
+start_of_sending = time.perf_counter()
 send_single_event_to_api(data[1])
+end_of_sending = time.perf_counter()
+print(f"Time taken to send single event: {end_of_sending - start_of_sending:.2f} seconds")
 time.sleep(1)
 
 print("Replaying all events 1000x faster")
